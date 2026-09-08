8824c3223880eaf2c43e71b3cdbff271dab70233
azaventure <ajgonefishin13@gmail.com>
2026-09-08 16:43:37 -0400

    Completed homework for 9-08-2026

---
+++ PNCMobileApp/PNCMobileApp/PNCMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PNCMobileAppApp.swift
+//  PNCMobileApp
+//
+//  Created by Aza Mohanty on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PNCMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
